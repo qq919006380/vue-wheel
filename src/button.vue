d3ec93caae83478a15a265479ddf2ce2f64bbe10
weibo <919006380@qq.com>
2018-11-28 21:34:50 +0800

    添加icon

--- src/button.vue
+++ src/button.vue
@@ -1,27 +1,55 @@
 <template>
-    <button><slot></slot></button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" @click='on'>
+        <svg class="icon" v-if=icon>
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content"><slot></slot></div>
+    </button>
 </template>
 
 <script>
-export default {};
+export default {
+  // props:['icon','iconPosition'],
+  props:{
+    "icon":{},
+    "iconPosition":{
+      type:String,
+      default:"left",
+      validator(value){
+        return (value === "left" || value === "right")
+      }
+    }
+  },
+  data:function(){
+    return{
+      data:true
+    }
+  },
+  methods:{
+    on:function(){
+      console.log(this.icon)
+    }
+  }
+  
+}
 </script>
 
 <style lang="scss">
 .g-button {
-  font-size: var(--font-size);
-  height: var(--button-height);
+  font-size: var(--font-size);height: var(--button-height);padding: 0 1em;border-radius: var(--border-raduis);
+  border: 1px solid var(--border-color);background: var(--button-bg);
+  display: inline-flex;justify-content: center;
+  align-items: center;vertical-align: bottom;
   padding: 0 1em;
-  border-radius: var(--border-raduis);
-  border: 1px solid var(--border-color);
-  background: var(--button-bg);
-  &:hover {
-    border-color: var(--border-color-hover);
-  }
-  &:active {
-    background-color: var(--button-active-bg);
-  }
-  &:focus {
-    outline: none;
+  &:hover {border-color: var(--border-color-hover);}
+  &:active {background-color: var(--button-active-bg);}
+  &:focus {outline: none;}
+  > .content{order:2}
+  > .icon{order:1;margin-right: .1em}
+  
+  &.icon-right{
+    > .content{order:1}
+    > .icon{order:2;margin-right: 0; margin-left: .1em}
   }
 }
 </style>
