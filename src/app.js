f11d6176b1b07443a54533cc51d0291020532488
weibo <919006380@qq.com>
2018-12-02 20:25:00 +0800

    使用chai.spy监听回调函数

--- src/app.js
+++ src/app.js
@@ -16,6 +16,8 @@ new Vue({
 
 // 单元测试
 import chai from 'chai';
+import spies from 'chai-spies';
+chai.use(spies)
 const expect = chai.expect
 {
     const Constructor = Vue.extend(Button)
@@ -78,4 +80,20 @@ const expect = chai.expect
     expect(order).to.eq('2')
     button.$el.remove()
     button.$destroy()
+}
+{
+    const Constructor=Vue.extend(Button)
+    const vm =new Constructor({
+        propsData:{
+            icon:"settings"
+        }
+    })
+    vm.$mount()
+    let spy=chai.spy(function(){})
+
+    vm.$on('click',spy)
+    let button =vm.$el
+    button.click()
+    expect(spy).to.have.been.called()
+    
 }
