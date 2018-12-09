d29d70c34997aabcc10c91ca877ec2e2cb814e98
weibo <919006380@qq.com>
2018-12-09 20:07:03 +0800

    基本实现row和col的功能

--- src/col.vue
+++ src/col.vue
@@ -1,32 +1,45 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
+  <div class="col" :class="[span && `col-${span}`,offset &&`offset-${offset}`]"
+  :style="{paddingLeft:gutter/2+'px',paddingRighe:gutter/2+'px'}">
+    <div style="border:1px solid green;height:100px">
         <slot></slot>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name:"w-col",
-    props:{
-        span:{
-            type:[Number,String]
-        }
+  name: "w-col",
+  props: {
+    span: {
+      type: [Number, String]
+    },
+    offset: {
+      type: [Number, String]
     }
-}
+  },
+  data(){
+      return{
+          gutter:0
+      }
+  }
+};
 </script>
 
 <style lang='scss' scoped>
-    .col{
-        height: 100px;
-        background:grey;
-        width: 50%;
-        border: 1px solid red;
-        $class-prefix:col-;
-        @for $n from 1 through 24{
-            &.#{$class-prefix}#{$n}{
-                width:($n/24)*100%
-            }
-        }
+.col {
+  width: 50%;
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n/24) * 100%;
     }
-
+  }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n/24) * 100%;
+    }
+  }
+}
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,12 +1,27 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft:gutter/2+'px',margunRight:-gutter/2+'px'}">
         <slot></slot>
     </div>
 </template>
 
 <script>
 export default {
-
+    name:"wRow",
+    props:{
+        gutter:{
+            type:[Number,String]
+        }
+    },
+    created(){
+        console.log('row created')
+    },
+    mounted(){
+        console.log('row mounted')
+        console.log(this.$children)
+        this.$children.forEach((vm)=>{
+            vm.gutter=this.gutter
+        })
+    }
 }
 </script>
 
