b6a735ddeb1d64facfc67d18056e72b493d3432a
weibo <919006380@qq.com>
2019-01-12 23:09:07 +0800

    添加官网文档

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/guide/' },
+        { text: 'External', link: 'https://google.com' },
+      ],
+      sidebar: [
+        ['/install/','安装'],
+        ['/get-started/', 'started'],
+        ['/components/','组件']
+      ]
+    }
+  }
