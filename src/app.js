27d448e59e3f663e488570701335fd8e2ec0ed29
weibo <919006380@qq.com>
2019-01-14 19:34:09 +0800

    添加cascader组件

--- src/app.js
+++ src/app.js
@@ -1,67 +1,62 @@
-import Vue from 'vue';
-import Button from './button';
-import Icon from './icon';
-import  './icon';
-import ButtonGroup from './button-group';
-import Input from './input';
-import Row from './row';
-import Col from './col';
-import Toast from './toast';
-import plugin from './plugin';
-import Tabs from './tabs';
-import TabsHead from './tabs-head';
-import TabsBody from './tabs-body';
-import TabsItem from './tabs-item';
-import TabsPane from './tabs-pane';
+import Vue from "vue";
+import Button from "./button";
+import Icon from "./icon";
+import ButtonGroup from "./button-group";
+import Input from "./input";
+import Row from "./row";
+import Col from "./col";
+import Toast from "./toast";
+import plugin from "./plugin";
+import Tabs from "./tabs";
+import TabsHead from "./tabs-head";
+import TabsBody from "./tabs-body";
+import TabsItem from "./tabs-item";
+import TabsPane from "./tabs-pane";
+import Cascader from "./cascader";
 
-Vue.component('g-button', Button)
-Vue.component('g-icon', Icon)
-Vue.component('g-button-group', ButtonGroup)
-Vue.component('g-inputp', Input)
-Vue.component('g-col', Col)
-Vue.component('g-row', Row)
-Vue.component('g-toast', Toast)
-Vue.component('g-tabs', Tabs)
-Vue.component('g-tabs-head', TabsHead)
-Vue.component('g-tabs-body', TabsBody)
-Vue.component('g-tabs-item', TabsItem)
-Vue.component('g-tabs-pane', TabsPane)
+Vue.component("g-button", Button);
+Vue.component("g-cascader", Cascader);
+Vue.component("g-icon", Icon);
+Vue.component("g-button-group", ButtonGroup);
+Vue.component("g-inputp", Input);
+Vue.component("g-col", Col);
+Vue.component("g-row", Row);
+Vue.component("g-toast", Toast);
+Vue.component("g-tabs", Tabs);
+Vue.component("g-tabs-head", TabsHead);
+Vue.component("g-tabs-body", TabsBody);
+Vue.component("g-tabs-item", TabsItem);
+Vue.component("g-tabs-pane", TabsPane);
 
-Vue.use(plugin)
+Vue.use(plugin);
 new Vue({
-    el: '#app',
-    data: {
-        selectedTab:"sports",
-        selected: 'angular'
-    },
-    created() {
-
-    },
-    methods: {
-        showToast1() {
-            this.showToast('top')
-        },
-        showToast2() {
-            this.showToast('middle')
-        },
-        showToast3() {
-            this.showToast('bottom')
-        },
-        showToast(position) {
-            this.$toast('123',
-                {
-                    closeButton: {
-                        text: "知道了",
-                        callback(toast) {
-                            console.log(toast)
-                            console.log("用户说他知道了")
-                        }
-                    },
-                    enableHtml: true,
-                    autoClose: 3,
-                    position: position
-                })
-        }
-    },
-
-})
+  el: "#app",
+  data: {
+    selectedTab: "sports",
+    selected: "angular",
+    source: [
+      {
+        name: "浙江",
+        children: [
+          {
+            name: "杭州",
+            children: [{ name: "上城" }, { name: "下城" }, { name: "江干" }]
+          },
+          {
+            name: "嘉兴",
+            children: [{ name: "南湖" }, { name: "秀洲" }, { name: "嘉善" }]
+          }
+        ]
+      },
+      {
+        name: "福建",
+        children: [
+          {
+            name: "福州",
+            children: [{ name: "鼓楼" }, { name: "台江" }, { name: "仓山" }]
+          }
+        ]
+      }
+    ]
+  }
+});
--- src/cascader.vue
+++ src/cascader.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="cascader">
-    <slot></slot>
+    <div class="trigger">
+      <slot></slot>
+    </div>
+    <div class="popover">
+      <div v-for="(item,i) in source" :key="i">
+        <cascader-item :source-item="item"></cascader-item>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import CascaderItem from './cascader-item'
   export default {
-    name: 'GuluCascader'
+    name: 'GuluCascader',
+    components: {CascaderItem},
+    props: {
+      source: {
+        type: Array
+      }
+    }
   }
 </script>
 
@@ -14,4 +28,4 @@
   @import "var";
   .cascader {
   }
-</style> 
+</style>
