818d33b8c9ab1d874a21aa51f3cc9bb7808126d7
qq919006380 <919006380@qq.com>
2019-01-15 11:50:05 +0800

    add input

---
+++ docs/.vuepress/components/input-demos.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <g-input></g-input>
+  </div>
+</template>
+
+<script>
+import Input from "../../../src/input";
+export default {
+  components: {
+    "g-input": Input
+  },
+  data() {
+    return {
+      content: `
+            <g-input></g-input>
+            `
+        .replace(/\t+| +/g, "")
+        .trim()
+    };
+  }
+};
+</script>
+
+<style>
+</style>
--- docs/components/input.md
+++ docs/components/input.md
@@ -1,1 +1,5 @@
 # input
+使用方法
+<ClientOnly>
+    <input-demos></input-demos>
+</ClientOnly>
