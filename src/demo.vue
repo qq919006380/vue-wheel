9e6677f8f1684da0c617b4529d0b68acbf960ae1
weibo <919006380@qq.com>
2019-02-16 17:25:06 +0800

    导入测试数据库

--- src/demo.vue
+++ src/demo.vue
@@ -3,7 +3,7 @@
     <p>{{selected &&selected[0] &&selected[0].name || '空'}}</p>
     <p>{{selected &&selected[1] &&selected[1].name || '空'}}</p>
     <p>{{selected &&selected[2] &&selected[2].name || '空'}}</p>
-    <g-cascader :source="source" popover-height='200px' :selected="selected" @update:selected='selected=$event'></g-cascader>
+    <g-cascader :source="source" popover-height="200px" :selected.sync="selected"></g-cascader>
     <p>222</p>
   </div>
 </template>
@@ -11,6 +11,11 @@
 <script>
 import Button from "./button";
 import Cascader from "./cascader";
+import db from "./db";
+function ajax(parentId = 0) {
+  return db.filter(item => item.parent_id == parentId);
+}
+console.log(ajax());
 export default {
   components: {
     "g-button": Button,
@@ -18,31 +23,8 @@ export default {
   },
   data() {
     return {
-      selected:[],
-      source: [
-        {
-          name: "浙江",
-          children: [
-            {
-              name: "杭州",
-              children: [{ name: "上城" }, { name: "下城" }, { name: "江干" }]
-            },
-            {
-              name: "嘉兴",
-              children: [{ name: "南湖" }, { name: "秀洲" }, { name: "嘉善" }]
-            }
-          ]
-        },
-        {
-          name: "福建",
-          children: [
-            {
-              name: "福州",
-              children: [{ name: "鼓楼" }, { name: "台江" }, { name: "仓山" }]
-            }
-          ]
-        }
-      ]
+      selected: [],
+      source:ajax()
     };
   }
 };
